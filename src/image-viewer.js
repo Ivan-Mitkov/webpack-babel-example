dbf58af7a98caa8def7901f518d8e73788ae5081
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-08-18 17:07:29 +0300

    A little late commit

---
+++ src/image-viewer.js
@@ -0,0 +1,8 @@
+import '../styles/image-viewer.css';
+
+const image = document.createElement('img');
+image.src = "http://lorempixel.com/400/400";
+image.width = 400;
+
+
+document.body.appendChild(image);
---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+import sum from './sum';
+import './image-viewer.js';
+
+console.log(sum(1, 3));
